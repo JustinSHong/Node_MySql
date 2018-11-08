5b4db06f151f3384b7aef8a5b0c5ce986c5f2ac3
JustinSHong <jhong2009@gmail.com>
2018-11-07 22:34:27 -0800

    created /createUser for POSTing new users

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
+app.post("/createUser", (req, res) => {
+    store
+        .createUser({
+            username: req.body.username,
+            password: req.body.password
+        })
+        .then(() => res.sendStatus(200));
+});
+
 app.listen(7555, () => {
     console.log(`\n=== Server running on http://localhost:7555 === \n`);
 });
