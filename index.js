30b129a10bbc5f623f6982e8a070d16b941aa799
JustinSHong <jhong2009@gmail.com>
2018-12-16 21:45:20 -0800

    successfully set up knex with mysql

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const store = require("./store");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
 app.post("/createUser", (req, res) => {
