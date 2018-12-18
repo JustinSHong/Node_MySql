813a5daeef9b104df3aeb2a9dbe305edfe34dc0e
JustinSHong <jhong2009@gmail.com>
2018-12-17 19:35:30 -0800

    implemented log in functionality

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 const CreateUser = document.querySelector(".CreateUser");
+const Login = document.querySelector(".Login");
 
 CreateUser.addEventListener("submit", e => {
     e.preventDefault();
@@ -6,6 +7,16 @@ CreateUser.addEventListener("submit", e => {
     const password = CreateUser.querySelector(".password").value;
     // post new user to the database
     post("/createuser", { username, password });
+});
+
+Login.addEventListener("submit", e => {
+    e.preventDefault();
+    const username = Login.querySelector(".username").value;
+    const password = Login.querySelector(".password").value;
+    post("/login", { username, password }).then(({ status }) => {
+        if (status === 200) alert("login success");
+        else alert("login failed");
+    });
 });
 
 function post(path, payload) {
